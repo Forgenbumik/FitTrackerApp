25a8e9a69777665508bbdd4e06b1c68ad1809d57
Ilya <ilyanosevich@gmail.com>
2025-01-08 22:11:05 +0300

    Исправление избыточности данных о связи упражнений и подходов

--- app/src/main/java/com/example/fittrackerapp/entities/Set.kt
+++ app/src/main/java/com/example/fittrackerapp/entities/Set.kt
@@ -2,11 +2,23 @@ package com.example.fittrackerapp.entities
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
+import kotlin.collections.Set
 
-@Entity(tableName = "sets")
+@Entity(
+    tableName = "sets",
+    foreignKeys = [
+        ForeignKey(
+            entity = CompletedExercise::class,
+            parentColumns = ["id"],
+            childColumns = ["completed_exercise_id"]
+        )
+    ]
+)
 data class Set(
     @PrimaryKey val id: Int,
+    @ColumnInfo(name = "completed_exercise_id") val completedExerciseId: Long,
     @ColumnInfo(name = "duration") val duration: Int,
     @ColumnInfo(name = "reps") val reps: Int,
     @ColumnInfo(name = "weight") val weight: Double
