0ff711c79e99ce9ed725f9b38cd1e11c52991b53
Ilya <ilyanosevich@gmail.com>
2025-01-08 22:14:07 +0300

    Добавление новых методов, рефакторинг

--- app/src/main/java/com/example/fittrackerapp/daoInterfaces/CompletedExerciseDao.kt
+++ app/src/main/java/com/example/fittrackerapp/daoInterfaces/CompletedExerciseDao.kt
@@ -3,17 +3,23 @@ package com.example.fittrackerapp.daoInterfaces
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
+import com.example.fittrackerapp.entities.Category
 import com.example.fittrackerapp.entities.CompletedExercise
+import kotlinx.coroutines.flow.Flow
 
 interface CompletedExerciseDao {
 
     @Insert(entity = CompletedExercise::class)
-    suspend fun insertNewCompletedExercise(completedExercise: CompletedExercise)
+    suspend fun insert(completedExercise: CompletedExercise)
 
     @Query("SELECT * FROM completed_exercises WHERE id = :completedExerciseId")
-    suspend fun getCompletedExerciseById(completedExerciseId: Int): CompletedExercise?
+    suspend fun getById(completedExerciseId: Int): CompletedExercise?
 
     @Delete
-    suspend fun deleteWorkout(completedExercise: CompletedExercise)
+    suspend fun delete(completedExercise: CompletedExercise)
+
+    @Update
+    suspend fun update(completedExercise: CompletedExercise)
 
 }
