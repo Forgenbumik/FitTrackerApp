20276ad3169a7110e909554534ddc642192b3e71
Ilya <ilyanosevich@gmail.com>
2025-01-08 22:04:51 +0300

    Классы для связывания тренировки и упражнений

--- app/src/main/java/com/example/fittrackerapp/classes/daointerfaces/WorkoutWithExercisesDao.kt
+++ app/src/main/java/com/example/fittrackerapp/classes/daointerfaces/WorkoutWithExercisesDao.kt
@@ -1,12 +1,10 @@
-package com.example.fittrackerapp.daoInterfaces
+package com.example.fittrackerapp.classes.daointerfaces
 
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
-import androidx.room.Transaction
 import com.example.fittrackerapp.entities.Workout
-import com.example.fittrackerapp.entities.WorkoutDetail
 
 @Dao
 interface WorkoutWithExercisesDao {
