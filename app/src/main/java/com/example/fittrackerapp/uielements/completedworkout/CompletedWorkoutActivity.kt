5b0a4cbf3bf597bdef39a757b3c12ca23e7592bf
Ilya <ilyanosevich@gmail.com>
2025-05-13 20:45:24 +0300

    вывод информации о записанной тренировке

--- app/src/main/java/com/example/fittrackerapp/uielements/completedworkout/CompletedWorkoutActivity.kt
+++ app/src/main/java/com/example/fittrackerapp/uielements/completedworkout/CompletedWorkoutActivity.kt
@@ -2,10 +2,12 @@ package com.example.fittrackerapp.uielements.completedworkout
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -20,6 +22,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -31,19 +34,23 @@ import com.example.fittrackerapp.ui.theme.FitTrackerAppTheme
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.fittrackerapp.entities.CompletedExercise
 import com.example.fittrackerapp.entities.CompletedExerciseRepository
+import com.example.fittrackerapp.entities.CompletedWorkout
 import com.example.fittrackerapp.entities.CompletedWorkoutRepository
 import com.example.fittrackerapp.uielements.completedexercise.CompletedExerciseActivity
 
 class CompletedWorkoutActivity: ComponentActivity()  {
     private lateinit var viewModel: CompletedWorkoutViewModel
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             FitTrackerAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    CompletedExerciseMainScreen(modifier = Modifier.padding(innerPadding), onExerciseClick = { exercise, exerciseName -> onExerciseClick(exercise, exerciseName) })
+                    MainScreen(modifier = Modifier.padding(innerPadding),
+                               onExerciseClick = { exercise, exerciseName -> onExerciseClick(exercise, exerciseName) },
+                               formatTime = ::formatTime)
                 }
             }
         }
@@ -67,19 +74,47 @@ class CompletedWorkoutActivity: ComponentActivity()  {
         }
         startActivity(intent)
     }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun formatTime(secs: Int): String {
+        return viewModel.formatTime(secs)
+    }
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun CompletedExerciseMainScreen(modifier: Modifier, viewModel: CompletedWorkoutViewModel = viewModel(), onExerciseClick: (CompletedExercise, String) -> Unit) {
+fun MainScreen(modifier: Modifier, formatTime: (Int) -> String, onExerciseClick: (CompletedExercise, String) -> Unit, viewModel: CompletedWorkoutViewModel = viewModel(), ) {
 
     val completedExercises = viewModel.completedExercises.collectAsState().value
+
     Column(
         modifier.windowInsetsPadding(WindowInsets.statusBars)
     ) {
+        WorkoutInformation()
         ExercisesList(completedExercises, onExerciseClick)
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun WorkoutInformation(viewModel: CompletedWorkoutViewModel = viewModel()) {
+
+    val completedWorkout = viewModel.completedWorkout.collectAsState().value
+
+    val formattedTime = viewModel.formatTime(completedWorkout.duration)
+    val totalExercises = viewModel.getWorkoutTotalExercises()
+    val totalSets = viewModel.getWorkoutTotalSets()
+    val totalReps = viewModel.getWorkoutTotalReps()
+
+    Column {
+        Text("Общее время: ${formattedTime}")
+        Text("Всего упражнений: ${totalExercises}")
+        Text("Всего подходов: ${totalSets}")
+        Text("Всего повторений: ${totalReps}")
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun ExercisesList(completedExercises: List<CompletedExercise>, onExerciseClick: (CompletedExercise, String) -> Unit) {
     LazyColumn {
@@ -89,15 +124,15 @@ fun ExercisesList(completedExercises: List<CompletedExercise>, onExerciseClick: 
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @SuppressLint("UnrememberedMutableState")
 @Composable
 fun ExerciseItem(completedExercise: CompletedExercise, onExerciseClick: (CompletedExercise, String) -> Unit, viewModel: CompletedWorkoutViewModel = viewModel()) {
 
     val exerciseName = remember {mutableStateOf("")}
-
     val exerciseSetsNumber = remember { mutableStateOf(0) }
-
     val exerciseTotalReps = remember { mutableStateOf(0) }
+
     LaunchedEffect(completedExercise.exerciseId) {
         exerciseName.value = viewModel.getExerciseName(completedExercise.exerciseId)
         exerciseSetsNumber.value = viewModel.getExerciseSetsNumber(completedExercise.id)
@@ -111,4 +146,3 @@ fun ExerciseItem(completedExercise: CompletedExercise, onExerciseClick: (Complet
         Text("${exerciseTotalReps.value} повторений")
     }
 }
-
--- app/src/main/java/com/example/fittrackerapp/uielements/completedworkout/CompletedWorkoutViewModel.kt
+++ app/src/main/java/com/example/fittrackerapp/uielements/completedworkout/CompletedWorkoutViewModel.kt
@@ -7,20 +7,31 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.example.fittrackerapp.entities.CompletedExercise
 import com.example.fittrackerapp.entities.CompletedExerciseRepository
+import com.example.fittrackerapp.entities.CompletedWorkout
 import com.example.fittrackerapp.entities.CompletedWorkoutRepository
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
+@RequiresApi(Build.VERSION_CODES.O)
 class CompletedWorkoutViewModel(
     private val completedWorkoutId: Long,
     private val completedWorkoutRepository: CompletedWorkoutRepository,
     private val completedExerciseRepository: CompletedExerciseRepository
 ): ViewModel() {
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    private val _completedWorkout = MutableStateFlow(CompletedWorkout())
+    @RequiresApi(Build.VERSION_CODES.O)
+    val completedWorkout: StateFlow<CompletedWorkout> = _completedWorkout
+
     private val _completedExercises = MutableStateFlow<List<CompletedExercise>>(emptyList())
     val completedExercises = _completedExercises
 
     init {
+        viewModelScope.launch {
+            _completedWorkout.value = completedWorkoutRepository.getById(completedWorkoutId)
+        }
         viewModelScope.launch {
             completedExerciseRepository.getByCompletedWorkoutIdFlow(completedWorkoutId).collect {
                 _completedExercises.value = it
@@ -45,6 +56,28 @@ class CompletedWorkoutViewModel(
 
     suspend fun getExerciseTotalReps(exerciseId: Long): Int {
         return completedExerciseRepository.getTotalReps(exerciseId)
+    }
+
+    fun getWorkoutTotalExercises(): Int {
+        return _completedExercises.value.size
+    }
+
+    fun getWorkoutTotalSets(): Int {
+        var totalSets = 0
+        viewModelScope.launch {
+            for (completedExercise in _completedExercises.value) {
+                totalSets += completedExerciseRepository.getSetsNumber(completedExercise.id)
+            }
+        }.let { return totalSets }
+    }
+
+    fun getWorkoutTotalReps(): Int {
+        var totalReps = 0
+        viewModelScope.launch {
+            for (completedExercise in _completedExercises.value) {
+                totalReps += completedExerciseRepository.getTotalReps(completedExercise.id)
+            }
+        }.let { return totalReps }
     }
 }
 
