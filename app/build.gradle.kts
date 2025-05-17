a0dffc1b5b69d39911b168f60f18f5c0799951aa
Ilya <ilyanosevich@gmail.com>
2025-05-17 17:03:13 +0300

    библиотеки для unit-тестов

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,8 +76,8 @@ dependencies {
     implementation(libs.androidx.media3.exoplayer)
     implementation(libs.androidx.media3.ui)
     implementation(libs.androidx.media3.common)
-    testImplementation( "junit:junit:4.13.2")
-    testImplementation("org.mockito:mockito-core:5.2.0")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
-    testImplementation("org.mockito.kotlin:mockito-kotlin:5.2.1")
+    testImplementation ("org.mockito:mockito-core:5.2.0")
+    testImplementation ("org.mockito.kotlin:mockito-kotlin:5.2.1")
+    testImplementation ("junit:junit:4.13.2")
 }
